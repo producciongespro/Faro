763273861715eb57219e864562986523a340afe2
lchaconc <luis.chacon.campos@gmail.com>
2019-03-28 11:22:33 -0600

    REc didácticos

--- src/Components/DocsOficiales.js
+++ src/Components/DocsOficiales.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import images from "../data/images.json";
+import textos from "../data/textos.json";
+
 
 
 
@@ -9,56 +12,55 @@ class DocsOficiales extends Component {
       <div>  
           <div className="row">
       <div className="col-4">
-            <img className="titulos img-fluid" src="images/tituloDocumentos.png" alt="Documentos educativos oficiales" />
-               <img className="descripciones img-fluid" src="images/descripcion.png" alt="Descripción" />
-              <div id="textoDescripcion">
-                El tacaco es una planta trepadora endémica de Costa Rica, familia de las cucurbitáceas, y que produce un fruto comestible verde, ovoide, de unos siete centímetros de longitud.​ La parte comestible es una pulpa contenida en una bolsa fibrosa dentro de la cual hay una pepita aplastada y amarga que se extrae previamente.
+            <img className="titulos img-fluid" src={images[0].DocsOficialesTitulo } alt="Documentos educativos oficiales" />               
+              <div id="textoDescripcion" className="row-text"  >
+                  {textos[0].tacaco  }
               </div>
-              <img id="programas" className="img-fluid" src="images/programas_estudio.png" alt="Descripción" />
+              <img id="programas" className="img-fluid" src= {images[0].GeneralProgramasEstudio }  alt="Descripción" />
       </div>
       <div className="col-8" >
         <div className="row">
             <div className="col-4 hover11 ladoIzq">
               <figure>
-                <img className="botones-portada img-fluid" id="politica" src="images\politica.png" alt="" />
+                <img className="botones-portada img-fluid" id="politica" src=  {images[0].DocsOficialesPoliticaEducativa } alt="Políticas educativas" />
               </figure>
               </div>
             <div className="col-4 hover11 centro">
                 <figure>
-              <img className="botones-portada img-fluid" id="perfil" src="images\docente.png" alt="" />
+              <img className="botones-portada img-fluid" id="perfil" src={images[0].DocsOficialesPerfildocente }  alt="Perfil docente" />
               </figure>
             </div>
             <div className="col-4 hover11 ladoDer">
                 <figure>
-              <img className="botones-portada img-fluid" id="estudiante" src="images\estudiante.png" alt="" />
+              <img className="botones-portada img-fluid" id="estudiante" src= {images[0].DocsOficialesPerfilEstudiante } alt="Perfil estudiante" />
               </figure>
             </div>
         </div>
         <div className="row">
             <div className="col-4"></div>
             <div className="col-4 fondo" >   
-              <img id="transformacion" className="img-fluid" src="images\transformacion.png" alt="" />
+              <img id="transformacion" className="img-fluid" src= {images[0].DocsOficialesTransformacionCurricular }   alt="Transformación curricular" />
             </div>
             <div className="col-4"></div>
         </div>
         <div className="row">
           <div className="col-4 hover11 ladoIzq">
               <figure>
-            <img className="botones-portada  img-fluid" id="faro" src="images\faro.png" alt="" />
+            <img className="botones-portada  img-fluid" id="faro" src=  {images[0].DocsOficialesFaro }  alt="Faro" />
             </figure>
           </div>
           <div className="col-4 hover11 centro">
               <figure>
-            <img className="botones-portada  img-fluid" id="fundamentos" src="images\fundamentos.png" alt="" />
+            <img className="botones-portada  img-fluid" id="fundamentos" src= {images[0].DocsOficialesFundCurriculo }  alt="Fundamentos Currículo" />
             </figure>
           </div>
           <div className="col-4 hover11 ladoDer">
               <figure>
-            <img className="botones-portada  img-fluid" id="reglamento" src="images\reglamento.png" alt="" />
+            <img className="botones-portada  img-fluid" id="reglamento" src={images[0].DocsOficialesReglamento }  alt="Reglamento" />
             </figure>
           </div>
           <div className="col-12 pie">
-            <img className="botones-portada img-fluid" id="" src="images\salir.png" alt="" />
+            <img  data-tar="Home" className="btn"  onClick={this.props.changePage}   className="btn img-fluid"  src= {images[0].BtnSalir }  alt="Salir" />
           </div>
         </div>
       </div>
--- src/Components/RecursosDidacticos.js
+++ src/Components/RecursosDidacticos.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import images from "../data/images.json";
 
 
 
@@ -6,14 +7,38 @@ import React, { Component } from 'react';
 class RecursosDidacticos extends Component {
   render() {
     return (
-      <div>  
-          <h1>Recursos Didácticos</h1>   
-          <br/>
-          
-          RecursosDidacticos...
-          <hr/>
-          <hr/>
-          <button data-tar="Home" className="btn btn-success"  onClick={this.props.changePage} > SALIR </button>
+      <div className="row">
+      <div className="col-4">
+            <img className="titulos img-fluid" src="images/tituloRecursos.png" alt="Documentos educativos oficiales" />
+               <img className="descripciones img-fluid" src="images/descripcion.png" alt="Descripción" />
+              <div id="textoDescripcion">
+                El tacaco es una planta trepadora endémica de Costa Rica, familia de las cucurbitáceas, y que produce un fruto comestible verde, ovoide, de unos siete centímetros de longitud.​ La parte comestible es una pulpa contenida en una bolsa fibrosa dentro de la cual hay una pepita aplastada y amarga que se extrae previamente.
+              </div>
+              <img id="programas" className="img-fluid" src ={images[0].GeneralEducatico } alt="Educatico" />
+      </div>
+      <div className="col-8">
+        <div className="row">
+            <div className="col-4 hover12 ladoIzq">
+              <figure>
+                <img className="botones-portada img-fluid" id="prescolar" src= {images[0].RecDidacticosPreescolar  } alt="Preescolar" />
+              </figure>
+              </div>
+            <div className="col-4 hover12 centro">
+                <figure>
+              <img className="botones-portada img-fluid" id="primaria" src= {images[0].RecDidacticosPrimaria  }   alt="Primaria" />
+              </figure>
+            </div>
+            <div className="col-4 hover12 ladoDer">
+                <figure>
+              <img className="botones-portada img-fluid" id="secundaria" src= {images[0].RecDidacticosSecundaria  }  alt="Secundaria" />
+              </figure>
+            </div>
+        </div>
+      
+          <div className="col-12 pie">
+            <img data-tar="Home" className="btn"  onClick={this.props.changePage}  className="btn img-fluid" id="salir" src= {images[0].BtnSalir  }   alt="Salir" />
+          </div>
+        </div>
       </div>
     );
   }
