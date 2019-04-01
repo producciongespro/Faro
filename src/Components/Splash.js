70be6fe3dec247a3385023e6ff8c84196be3c4a0
lchaconc <luis.chacon.campos@gmail.com>
2019-04-01 11:06:00 -0600

    mejora GUI splash

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -6,13 +6,25 @@ import React, { Component } from 'react';
 class Splash extends Component {
   render() {
     return (
-      <div>     
-          <img src={this.props.urlImage} alt="Logo Faro" />
-          <br/>
-          Espere un momento por favor...
+        <div className="container bg-white mt-5">
+        <br/><br/><br/>
+      <div className="row ">     
+          <div className="col-12 text-center"  >
+              <img src={this.props.urlImage} alt="Logo Faro" />              
+          </div>
       </div>
+      <br/> <br/> <br/>
+        <div className="row">     
+        <div className="col-12 text-center">
+                Espere un momento por favor...       
+        </div>
+        </div>
+        <br/><br/><br/>
+        </div>
     );
   }
 }
 
 export default Splash;
+              
+              
