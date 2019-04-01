ce4f5fd1d2fc35dc1dd696dc0e45367dbe6f28ee
lchaconc <luis.chacon.campos@gmail.com>
2019-04-01 10:58:29 -0600

    botones home
    audio
    volver
    caja

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,10 +7,12 @@ import images from "../data/images.json";
 class Home extends Component {
   render() {
     return (
-      <div>
+      
 
 <div className="row">
-      <div className="col-1"></div>
+      <div className="col-1">
+        <img className="btn"  src={ images[0].HomeVolver } data-tar="Portada"  onClick={this.props.changePage} alt="Volver a Portada" />
+      </div>
             <div className="col-10 ">
               <img alt="maestra"  id="principal" src={images[0].Maestra  } />
               <img alt="Apoyos para el clima del aula"   data-tar="ApoyoClimaAula"  onClick={this.props.changePage}      id="gafas" className="cliqueables" src={images[0].Gafas  }  />
@@ -20,12 +22,14 @@ class Home extends Component {
               <img alt="Desarrollo profesional"  id="celular" data-tar="DesarrolloProf" onClick={this.props.changePage}  className="cliqueables2" src={images[0].Celular  } />
               <img alt="Apoyos para el planeamiento" id="bolso" data-tar="ApoyosPlan"  onClick={this.props.changePage}  className="cliqueables" src={images[0].Bolso  }  />
             </div>
-      <div className="col-1"></div>
+      <div className="col-1">
+        <img className="btn" src={ images[0].HomeSonido } alt="Sonido" />
+        <img className="btn" src={ images[0].HomeCaja } alt="Caja herramientas" />
+      </div>
     </div>
-      <div className="row">
-      </div>
+    
 
-      </div>
+      
     );
   }
 }
