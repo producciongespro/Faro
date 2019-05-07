0d5f71c56640609c35c30efe589493cd8991c04d
lchaconc <luis.chacon.campos@gmail.com>
2019-05-07 14:55:05 -0600

    imp Catalogo fase 2
    Se generarn solo los priemros 4 objetos del array.
    Se debe cambiar a contendor para trabajar con estados

--- src/Components/Catalogo.jsx
+++ src/Components/Catalogo.jsx
@@ -5,19 +5,43 @@ import CardsImage from "../Comp-primitive/CardsImage.jsx";
 const datos = [
     {
       urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
-      title : "Montaña",
+      title : "Montaña 0 ",
       desc :  "Es una bella montaña",
       urlWeb : "lo.com"
     },
     {
       urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
-      title : "Montaña",
+      title : "Montaña 1",
       desc :  "Es una bella montaña",
       urlWeb : "lo.com"
     },
     {
       urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
-      title : "Montaña",
+      title : "Montaña 2 ",
+      desc :  "Es una bella montaña",
+      urlWeb : "lo.com"
+    },
+    {
+      urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
+      title : "Montaña 3",
+      desc :  "Es una bella montaña",
+      urlWeb : "lo.com"
+    },
+    {
+      urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
+      title : "Montaña 4",
+      desc :  "Es una bella montaña",
+      urlWeb : "lo.com"
+    },
+    {
+      urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
+      title : "Montaña 5",
+      desc :  "Es una bella montaña",
+      urlWeb : "lo.com"
+    },
+    {
+      urlImage : "https://www.ecestaticos.com/imagestatic/clipping/b05/b5b/b05b5bd1b9eec88c912908db22159143/las-misteriosas-montanas-africanas-en-las-que-se-produjo-el-genesis-de-la-vida.jpg",
+      title : "Montaña 6",
       desc :  "Es una bella montaña",
       urlWeb : "lo.com"
     }
@@ -25,21 +49,34 @@ const datos = [
 ]
 
 
+var cont = 0;
+var limite = 4;
 
+var sllicedArray = datos.slice(cont, cont + limite );
 
 
 const Catalogo = () => {
-    return (          
-  <div className="row">  
+    return ( 
+      
+      <React.Fragment>
+  <div className="row"> 
+    
  {
-      datos.map( (dato, i) => {
+      sllicedArray.map( (dato, i) => {
         return (
           <CardsImage urlImage={dato.urlImage }     title={dato.title}  desc={dato.desc}   urlWeb={dato.urlImage} key={i} />
         )
 })
-}   
+}  
+      
+    </div> 
+    
+    <div className="row">
+  <button className="btn btn-success">siguiente</button>
 
-  </div>        
+    </div>
+    </React.Fragment>
+    
      );
 }
  
