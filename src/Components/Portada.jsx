438c5239041dc3b3063017df9cab0e93896ca97f
arkoscr <32460304+arkoscr@users.noreply.github.com>
2019-05-08 12:42:46 -0600

    Ajustes en portada segun nuevas orientaciones

--- src/Components/Portada.jsx
+++ src/Components/Portada.jsx
@@ -13,25 +13,29 @@ const Portada = (props) => {
             </div>
         </div>
         <br/>
-            
+        <br/>    
             <div className="row" >
-                    <div className="col-6">                    
-                        <img onClick = {props.showModal }  data-typecontent ="video" data-content= {textos[0].VideoMinistro } className="img-fluid botones-portada"   src={images[0].BtnBienvenida} alt="Bienvenida"/>
+         
+                    <div className="col-5 text-right">                        
+                    <img onClick = {props.showModal }  data-typecontent ="video" data-content= {textos[0].VideoMinistro } className="img-fluid botones-portada"   src={images[0].BtnBienvenida} alt="Bienvenida"/>
                     </div>
-                    <div className="col-6 text-right">                        
-                        <img  data-content= {audios[0].Portada }  data-typecontent ="audio"  onClick = {props.showModal }  className="img-fluid botones-portada " src={images[0].BtnAudio} alt="Audio"/>
+                    <div className="col-2 ">                     
+                   
                     </div>
+                    <div className="col-5 text-left">                     
+                    <img  data-tar="Home"  onClick={props.changePage}  className="img-fluid botones-portada " src={images[0].BtnIngresar} alt="Ingresar"/>
+                    </div>
+      
             </div>
-            <br/>
-            <div className="row" >
-                    <div className="col-6 ">                     
-                        <img  data-content={textos[0].IndicacionesPortada  } data-typecontent="html"   onClick = {props.showModal }  className="img-fluid botones-portada" src={images[0].BtnIndicaciones } alt="Indicaciones"/>
-                    </div>
-                    <div className="col-6 text-right">                        
-                        <img  data-tar="Home"  onClick={props.changePage}  className="img-fluid botones-portada " src={images[0].BtnIngresar} alt="Ingresar"/>
-                    </div>
-            </div>
+        <div className="row">
      
+        <div className="col-12 text-center pie-pagina">
+           <br/>
+        <br/>
+        INSTITUTO DE DESARROLLO PROFESIONAL ULADISLAO GÁMEZ SOLANO - DIRECCIÓN DE RECURSOS TECNOLÓGICOS EN EDUCACIÓN - DIRECCIÓN DE DESARROLLO CURRICULAR
+        </div>
+       
+        </div>
         </React.Fragment>
      );
 }
