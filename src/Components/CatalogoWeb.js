5354275787388070f2a1c3f4a28064fa5332e0cf
lchaconc <luis.chacon.campos@gmail.com>
2019-05-10 14:11:44 -0600

    mejora css nav

--- src/Components/CatalogoWeb.js
+++ src/Components/CatalogoWeb.js
@@ -9,11 +9,11 @@ const CatalogoWeb = (props) => {
     return ( 
         <React.Fragment>
             <nav className="nav nav-pills fixed-top nav-justified custom-nav">
-            <a className="nav-item nav-link active" href="#educativos">Sitios Educativos</a>
-            <a className="nav-item nav-link" href="#web">Herramientas Web</a>
-            <a className="nav-item nav-link" href="#apps">Apps Educativas</a>            
-            <a className="nav-item nav-link" href="#estrategias">Estrategias de aprendizaje</a>            
-            <a className="nav-item nav-link" href="#tmp"  onClick={props.handlerCloseCatalogWeb }  >Volver</a>            
+            <a className="nav-item nav-link ink-custom" href="#educativos">Sitios Educativos</a>
+            <a className="nav-item nav-link ink-custom" href="#web">Herramientas Web</a>
+            <a className="nav-item nav-link ink-custom" href="#apps">Apps Educativas</a>            
+            <a className="nav-item nav-link ink-custom" href="#estrategias">Estrategias de aprendizaje</a>            
+            <a className="nav-item nav-link ink-custom" href="#tmp"  onClick={props.handlerCloseCatalogWeb }  >Volver</a>            
             </nav>
             <br/>
 
