3158e4b738660c2f7f8a5fbb3da6ae2d03a182b4
Patricia Hernandez Conejo <phernandezc@intranet.mep.go.cr>
2019-05-13 15:09:39 -0600

    cambio tarjetas
    elimina color y se agrega descripción

--- src/Comp-primitive/Cards.jsx
+++ src/Comp-primitive/Cards.jsx
@@ -13,18 +13,13 @@ const CardsImage = (props) => {
                 <div className="row">
                                   
                     <div className="card-body col-6">
-                        <img alt="prueba" className="card-img-top" src={props.item.url} />
-                      <div className="row">
-                        <div className="amarillo"></div>
-                        <div className="negrito"></div>
+                        <img alt="prueba" className="card-img-top" src={props.item.icono} />
                       </div>
-                    </div>
                
                     <div className="col-4 inform">
                         <h5 className="card-title"><b>Nombre: </b>{props.item.nombre}</h5>
                         <p className="card-text"><b>Dirigido a: </b> {props.item.meta}  </p>
-                        <p className="card-text"><b>Propósito: </b> {props.item.proposito}  </p>
-                        <p className="card-text"><b>Facilitador: </b> {props.item.facilitador}  </p>
+                        <p className="card-text"><b>Descripción: </b> {props.item.proposito}  </p>
                         <a href={props.item.url } className="link-card"  target="_blank" rel="noopener noreferrer" > > Ir al recurso </a>
                     </div>
 
