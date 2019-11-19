d71c37bdad27ff389b0ca76233fca3f45c501d7d
Patricia Hernandez Conejo <phernandezc@intranet.mep.go.cr>
2019-11-19 09:22:26 -0600

    Estilos hoja RECURSOS

--- src/Components/RecursosDidacticos.jsx
+++ src/Components/RecursosDidacticos.jsx
@@ -30,7 +30,7 @@ const RecursosDidacticos = (props) => {
 
       <div className="botones_salir col-12">
             <img  data-tar="Home" onClick={props.changePage}   className="derecha btn img-fluid hvr-pop"  src= {images[0].BtnSalir }  alt="Salir" />
-          </div>
+      </div>
           
       <div id="bnt-recursos" className="col-8">
         <div className="row">
@@ -51,20 +51,26 @@ const RecursosDidacticos = (props) => {
             </div>
         </div>
 
-        <div className="row">
+        <div  className="row">
           <div className="col-4 hover12 ladoDer">
+          <figure>
           <img className="btn img-fluid" id="jovenesAdultos" src= {images[0].RecDidacticosJovenesAdultos}   data-infosource={4} onMouseOver={props.onMouseOver}   onMouseOut={ props.onMouseOut }  onClick={props.handlerOpenBuscador}  alt="Jovenes y Adultos" />
+          </figure>
           {
             //<button id="intercultural" className="btn btn-success" onClick={props.handlerOpenBuscador} >Educación Intercultural</button>
           }
            
           </div>
           <div className="col-4 hover12 centro">
+          <figure>
           <img className="btn img-fluid" id="feriaCientifica" src= {images[0].RecDidacticosFeriaCientifica}   data-infosource={4} onMouseOver={props.onMouseOver}   onMouseOut={ props.onMouseOut }  onClick={props.handlerOpenBuscador}  alt="Feria Científica" />
+          </figure>
           </div>
 
           <div className="col-4 hover12 ladoIzq">
+          <figure>
           <img className="btn img-fluid" id="banderaAzul" src= {images[0].RecDidacticosBanderaAzul}   data-infosource={4} onMouseOver={props.onMouseOver}   onMouseOut={ props.onMouseOut }  onClick={props.handlerOpenBuscador}  alt="Bandera Azul" />
+          </figure>
           </div>
         </div>
       
