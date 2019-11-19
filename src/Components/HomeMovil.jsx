72f721359d521248552eec9d84745de57bd3991f
lchaconc <luis.chacon.campos@gmail.com>
2019-11-19 11:14:38 -0600

    botones encabezado home movil

--- src/Components/HomeMovil.jsx
+++ src/Components/HomeMovil.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import imagesJson from "../data/images.json";
+import textosJson from "../data/textos.json";
+
+const images = imagesJson[0];
+const textos = textosJson[0];
 
 const HomeMovil = (props) => {
     return (
         <div className="container">
-            <div className="jumbotron">
-                <h1> Caja de Herramietnas</h1>
+            <div className="jumbotron text-center">
+                <h1> Caja de Herramientas</h1>
+                <img className="btn img-fluid hvr-pop" id="indicaciones" src={ images.HomeCaja } onClick = {props.showModal }  data-typecontent ="help" data-content= {textos.IndicacionesPortada}  alt="Indicaciones" /> <br/>
+                <img className="btn img-fluid hvr-pop" id="home"  src={ images.HomeVolver } data-tar="Portada"  onClick={props.changePage} alt="Volver a Portada" /><br/>
             </div>
             <div className="row">
                 <div className="col-12 text-center">
