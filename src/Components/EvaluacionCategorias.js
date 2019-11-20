b1688d27bee68bdc25e76bd983140d23d256330c
lchaconc <luis.chacon.campos@gmail.com>
2019-11-20 08:27:41 -0600

    fase 1 imp evaluacion
    estructura básica y navegación

---
+++ src/Components/EvaluacionCategorias.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import images from '../data/images.json';
+
+class EvaluacionCategorias extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>
+                    {this.props.origen}
+                    Prueba Evaluacion
+                    </h1>                    
+                </div>
+                <div className="row">
+                    <div className="col-12 text-right">
+                        <img className="botones-portada hvr-pop img-fluid derecha  boton-volver" onClick={this.props.handlerCerrarCategoriasEvaluacion} src={images[0].BtnVolver} alt="Volver" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                        <button className="btn btn-success"> Prueba </button>
+                    </div>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default EvaluacionCategorias;
