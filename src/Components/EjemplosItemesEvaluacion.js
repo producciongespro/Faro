e5f10fbaad83e336b4cd77ed885c15eca1103112
lchaconc <luis.chacon.campos@gmail.com>
2019-11-20 15:04:32 -0600

    fase 2 imp ejemplos de itemes

--- src/Components/EjemplosItemesEvaluacion.js
+++ src/Components/EjemplosItemesEvaluacion.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 import images from '../data/images.json';
-//import categoriasEvaluacion from '../data/evaluacion/categorias_evaluacion.json';
+import ejemplosItemesPrimaria from '../data/evaluacion/ejemplos_itemes_primaria.json';
+import ejemplosItemesSecundaria from '../data/evaluacion/ejemplos_itemes_secundaria.json';
+
+//console.log("ejemplosItemesPrimaria",ejemplosItemesPrimaria[0]["Estudios sociales"] );
+
 
 class EjemplosItemesEvaluacion extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            asignaturas : null,
+            asignatura : null 
+        }
     }
+    ejemplosItemesJson = "";
+    materiasPrimaria = ["Ciencias", "Francés", "Italiano", "Inglés", "Matemática" ];
+    materiasSecundaria = ["Ciencias", "Estudios Sociales", "Cívica", "Francés", "Inglés", "Itaiano", "Biología",  "Español" ]
+
+    handlerCargarAsignatura =(e)=> {
+        const nivel = e.target.value;
+        switch (nivel) {
+            case "primaria":
+                this.setState({ asignaturas: this.materiasPrimaria });
+                this.ejemplosItemesJson = ejemplosItemesPrimaria;
+            break;
+            case "secundaria":
+                this.setState({ asignaturas: this.materiasSecundaria });
+                this.ejemplosItemesJson = ejemplosItemesSecundaria;    
+            break;
+        
+            default:
+                console.log("Opcion fuera de rango");                
+            break;
+        }
+    }
+
+    handlerSeleccionarAsignatura =(e)=>{
+        const valor = e.target.value;
+        const itemes = this.ejemplosItemesJson[0][valor];
+        console.log("itemes", itemes);
+        
+    }
+
+
     render() { 
         return ( 
             <div className="container">
@@ -19,7 +56,25 @@ class EjemplosItemesEvaluacion extends Component {
                     </div>
                 </div>
                 <div className="row">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate omnis saepe sint molestias quia nihil rerum optio nemo. Exercitationem, veniam. Rerum quisquam autem eveniet tempore doloribus quod hic quibusdam error?
+                 <div className="col-2">
+                    <select onClick={this.handlerCargarAsignatura}>
+                        <option value="" defaultValue disabled > Seleccione un nivel </option>
+                        <option value="primaria"> Primaria </option>
+                        <option value="secundaria"> Secundaria </option>
+                    </select>
+                 </div> 
+                 <div className="col-2">
+                    <select onClick={this.handlerSeleccionarAsignatura} >
+                        <option value="" defaultValue disabled> Seleccione una asginatura </option>
+                        {
+                            this.state.asignaturas != null && (
+                                this.state.asignaturas.map((item, i)=>(
+                                    <option key={"asignatura"+i  } value={item}> {item}  </option>
+                                )) 
+                            )
+                        }                       
+                    </select>
+                 </div>                
                 </div>
             </div>
          );
