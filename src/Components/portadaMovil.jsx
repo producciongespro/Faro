68e7ddd8e409ef5abff32d8d6527d5f5042a078a
arkoscr <tutordejava@gmail.com>
2019-11-21 13:52:30 -0600

    PortadaMovil

--- src/Components/portadaMovil.jsx
+++ src/Components/portadaMovil.jsx
@@ -9,32 +9,35 @@ const images = imagesJson[0];
 const PortadaMovil = (props) => {
     return ( 
         <React.Fragment>
-        <div className="row">
-            <div className="col-12 text-center">
-                <img id="img-portada" src={images.TituloPortada   } className="img-fluid" alt="Título portada"/>
+            <div className="row">
+                <div className="col-12 text-center">
+                    <img id="img-portada" src={images.logoMEPCH   } className="img-fluid" alt="Título portada"/>
+                </div>
             </div>
-        </div>
-  
             <div className="row" >
-         
-                    <div className="col-5 text-right">                        
+                <div className="col-2 ">                     
+                </div>
+                <div className="col-8 text-right">                        
                     <img onClick = {props.showModal }  data-typecontent ="video" data-content= {textos.VideoMinistro } className="img-fluid botones-portada hvr-pop"   src={images.BtnBienvenida} alt="Bienvenida"/>
-                    </div>
-                    <div className="col-2 ">                     
-                   
-                    </div>
-                    <div className="col-5 text-left">                     
+                </div>
+                <div className="col-2 ">                     
+                </div>         
+            </div> 
+            <div className="row" >
+                <div className="col-2 ">                     
+                </div>
+                <div className="col-5 text-left">                     
                     <img  data-tar="Home"  onClick={props.changePage}  className="img-fluid botones-portada hvr-pop" src={images.BtnIngresar} alt="Ingresar"/>
-                    </div>
-      
+                </div>
+                <div className="col-2 ">                     
+                </div>
             </div>
         <div className="row">
-     
-        <div className="col-12 text-center pie-pagina">
-           <br/>
-        <br/>
-        INSTITUTO DE DESARROLLO PROFESIONAL ULADISLAO GÁMEZ SOLANO - DIRECCIÓN DE RECURSOS TECNOLÓGICOS EN EDUCACIÓN - DIRECCIÓN DE DESARROLLO CURRICULAR
-        </div>
+            <div className="col-12 text-center pie-pagina">
+                <br/>
+                <br/>
+                INSTITUTO DE DESARROLLO PROFESIONAL ULADISLAO GÁMEZ SOLANO - DIRECCIÓN DE RECURSOS TECNOLÓGICOS EN EDUCACIÓN - DIRECCIÓN DE DESARROLLO CURRICULAR
+            </div>
        
         </div>
         </React.Fragment>
