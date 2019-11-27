19ac95a3e01bfcb892211d1e24974a41d5b6be80
lchaconc <luis.chacon.campos@gmail.com>
2019-11-27 07:48:35 -0600

    cambio de ref  assets en portada
    se cambia las imagenes

--- src/Components/Portada.jsx
+++ src/Components/Portada.jsx
@@ -2,29 +2,34 @@ import React from 'react';
 //json:
 import imagesJson from "../data/images.json";
 import textosJson from "../data/textos.json";
+import assets from '../data/config/config.json';
+
 
 const textos = textosJson[0];
 const images = imagesJson[0];
+
+const img = assets.img;
+
 
 const Portada = (props) => {
     return ( 
         <React.Fragment>
         <div className="row">
             <div className="col-12 text-center">
-                <img id="img-portada" src={images.TituloPortada   } className="img-fluid" alt="Título portada"/>
+                <img id="img-portada" src={img.portada + "titulo.png"  } className="img-fluid" alt="Título portada"/>
             </div>
         </div>
   
             <div className="row" >
          
                     <div className="col-5 text-right">                        
-                    <img onClick = {props.showModal }  data-typecontent ="video" data-content= {textos.VideoMinistro } className="img-fluid botones-portada hvr-pop"   src={images.BtnBienvenida} alt="Bienvenida"/>
+                    <img onClick = {props.showModal }  data-typecontent ="video" data-content= {textos.VideoMinistro } className="img-fluid botones-portada hvr-pop"   src={img.portada + "btn_bienvenida.png"} alt="Bienvenida"/>
                     </div>
                     <div className="col-2 ">                     
                    
                     </div>
                     <div className="col-5 text-left">                     
-                    <img  data-tar="Home"  onClick={props.changePage}  className="img-fluid botones-portada hvr-pop" src={images.BtnIngresar} alt="Ingresar"/>
+                    <img  data-tar="Home"  onClick={props.changePage}  className="img-fluid botones-portada hvr-pop" src={img.portada + "btn_ingresar.png"} alt="Ingresar"/>
                     </div>
       
             </div>
