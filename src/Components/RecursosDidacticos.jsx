3db4129b1550b2967a53b47f857d3323f5405a53
Patricia Hernandez Conejo <phernandezc@intranet.mep.go.cr>
2019-11-27 15:46:28 -0600

    Imagen Coleccion  GESPRO
    Se agrega la imagen de los recursos GESPRO

--- src/Components/RecursosDidacticos.jsx
+++ src/Components/RecursosDidacticos.jsx
@@ -26,7 +26,7 @@ const RecursosDidacticos = (props) => {
               </div>
               
               <a href={links.Educatico} target="_blank" rel="noopener noreferrer"  >
-                  <img id="material_adicional" className="img-fluid btn hvr-wobble-bottom" src={images[0].GeneralEducatico } alt="Referenciar COLECCION GESPRO" />
+                  <img id="material_adicional" className="img-fluid btn hvr-wobble-bottom" src={img + "coleccion_gespro.png" } alt="Referenciar COLECCION GESPRO" />
               </a>
       </div>
                
