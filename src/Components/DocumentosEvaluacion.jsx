e67beebb723647e74e85519a00f0341b20941fae
Patricia Hernandez Conejo <phernandezc@intranet.mep.go.cr>
2019-12-04 09:33:25 -0600

    Encabezados de Documentos

--- src/Components/DocumentosEvaluacion.jsx
+++ src/Components/DocumentosEvaluacion.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import assets from '../data/config/config.json';
 
 
-const img = assets.img.general;
+const img = assets.img.apoyosEvaluacion;
+const imgGeneral = assets.img.general;
+
 console.log("img", img);
 
 
 const DocumentosEvaluacion = (props) => {
     return ( 
     <div className="container">
-        <div className="jumbotron">
-            <h1>Documentos</h1>            
-        </div>
-        <div className="row">
-            <div className="col-12 text-right">
-                <img  onClick={props.handlerCerrarDocumentosEvaluacion} src={ img + "btn_volver.png" } alt="Botón volver" role="Button" />
+            <div className="row">
+                <div className="col-12  text-right alert alert-secondary">
+                    <img className="bannerRecursos" src={ img + "encabezado_documentos.png"}  alt="Encabezado de Documentos de apoyo" />
+                    <img className="botones-portada hvr-pop img-fluid derecha  boton-volver" onClick={props.handlerCerrarDocumentosEvaluacion} src={ imgGeneral + "btn_volver.png" } alt="Volver" />
+                </div>
             </div>
+
+
+        <div className="row">
+            
         </div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perferendis qui libero facere ipsa, molestiae reprehenderit accusamus esse nulla suscipit, quod veniam minima! Soluta quae ad veritatis cum, saepe esse?
     </div> 
