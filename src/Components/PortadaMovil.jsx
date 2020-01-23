5b4739baf9dacc4256ce9a0aaa46cada317cdf5d
arkoscr <tutordejava@gmail.com>
2020-01-23 10:45:36 -0600

    config_m json y botón CC

--- src/Components/PortadaMovil.jsx
+++ src/Components/PortadaMovil.jsx
@@ -5,6 +5,7 @@ import textosJson from "../data/textos.json";
 import assets from "../data/config/config_m.json";
 const textos = textosJson[0];
 const img  = assets.img.portada;
+const imgGeneral = assets.img.general;
 //Portada en caso de móviles
 const PortadaMovil = (props) => {
     return ( 
@@ -39,7 +40,14 @@ const PortadaMovil = (props) => {
                 
             DIRECCIÓN DE DESARROLLO CURRICULAR - DIRECCIÓN DE RECURSOS TECNOLÓGICOS EN EDUCACIÓN - INSTITUTO DE DESARROLLO PROFESIONAL ULADISLAO GÁMEZ SOLANO
             </div>
-       
+    
+            <div className="col-12 text-center"><br/>
+        <a  href="https://creativecommons.org/licenses/by-nc-sa/3.0/cr/" target="_blank"> <img id="img-licencia" src={imgGeneral + "licencia.png"  } className="img-fluid" alt="Licencia Creative"/></a>
+        <br/> 
+        <p className="creative">Caja de Herramientas by Costa Rica.  Ministerio de Educación Pública. is licensed under a Creative Commons Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional License. <br/>
+        Creado a partir de la obra en https://www.mep.go.cr/educatico/caja. <br/>  Puede hallar permisos más allá de los concedidos con esta licencia en www.cajadeherramientas.mep.go.cr
+        </p>
+        </div>
         </div>
         </React.Fragment>
      );
