35d9011a161703c5c2dedf801c7b82c2a51601e8
Patricia Hdz <32460268+patyhc4oct@users.noreply.github.com>
2020-03-04 14:18:07 -0600

    Revert "Merge pull request #1 from producciongespro/imp-recursos-BD-Fase1"
    This reverts commit aa0a863ecc366c100c84b092a19d60bf6f3a2c6c, reversing
    changes made to 160de4eec3a02f597a00a102ed728d54d955509f.

--- src/modulos/obtener.js
+++
@@ -1,9 +0,0 @@
-async function obtener (url) {
-    const response = await fetch(url);
-    const json = await response.json();
-    //console.log(json);
-    return json;
-  }
-  
-  
-  export default obtener;
