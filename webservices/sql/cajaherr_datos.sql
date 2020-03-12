5c07ff6198c5242822214a89551800f67fc9d19f
lchaconc <luis.chacon.campos@gmail.com>
2020-03-12 11:51:31 -0600

    Actualizacion 2 SQL BD

--- webservices/sql/cajaherr_datos.sql
+++ webservices/sql/cajaherr_datos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 12-03-2020 a las 17:47:36
+-- Tiempo de generación: 12-03-2020 a las 17:50:57
 -- Versión del servidor: 5.7.21
 -- Versión de PHP: 7.1.16
 
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `cajaherr_datos`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+DROP TABLE IF EXISTS `bitacora`;
+CREATE TABLE IF NOT EXISTS `bitacora` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identificador del evento o acción realizada',
+  `id_usuario` varchar(128) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Usuario que realiza el evento',
+  `evento` varchar(128) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Acción realizada',
+  `fecha_evento` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Fecha en que sucede el evento',
+  `id_registro` int(11) NOT NULL COMMENT 'Identificador el ingreso modificado',
+  `tabla` varchar(256) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Nombre de la tabla a la que pertenece el registro afectado',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Tabla para documentar eventos de los usuarios en el sistema';
+
+--
+-- Volcado de datos para la tabla `bitacora`
+--
+
+INSERT INTO `bitacora` (`id`, `id_usuario`, `evento`, `fecha_evento`, `id_registro`, `tabla`) VALUES
+(1, '106', 'Agrega recurso', '2020-03-10 15:34:16', 1, 'recursos');
 
 -- --------------------------------------------------------
 
