265bab59cbde22a3cb4ac7a2f4e7c681e0d5fad9
lchaconc <luis.chacon.campos@gmail.com>
2020-06-10 14:38:01 -0600

    Fase 2 imp Pefagogia hosp
    -Renderizado de img encabezdo y boton

--- src/Components/PedagoHosp.js
+++ src/Components/PedagoHosp.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import config from '../data/config/config.json';
 const imgGenerales = config.img.general;
+const img = config.img.recursosDidacticos;
 
 function PedagogHosp(props) {
     
 
     return (
-            <div className="container">
-                <h1>Pedagogía hospitalaria</h1>
+        <React.Fragment>
+            <div className="row">
+                <div className="col-sm-12">
+                <img className="bannerRecursos"   src={ img + "ch_encabezado_pedagogiahospitalaria.png"} alt="Encabezado pedagogía hospitalaria" />
                 <img data-tar="Home" onClick={props.handleCerrarPedagoHosp} className="botones-portada hvr-pop boton-volver img-fluid derecha" src={imgGenerales + "btn_volver.png"} role="button" alt="Volver" /> 
+                </div>
             </div>
+        </React.Fragment>
     );
 }
 
