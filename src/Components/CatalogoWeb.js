41add5012c0936bbde8371185a7bb06a10f1ccf3
lchaconc <luis.chacon.campos@gmail.com>
2020-08-21 10:56:18 -0600

    Fase 3 recursos ODP
    -Filtrado de select
    -Imp de estados
    -Renderizado de registros

--- src/Components/CatalogoWeb.js
+++ src/Components/CatalogoWeb.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import sitiosWeb from '../data/desarrollo/sitios.json'
+import React, {useState} from 'react';
 import images from "../data/images.json";
 import filtrar from '../modulos/filtrar';
 
@@ -10,6 +9,7 @@ var img = images[0];
 //console.log(sitiosWeb);
 var recursosWeb=null;
 var subcategoriasODP=null;
+var subCategoria=null;
 
 const cargarRecursosWeb=()=>{
     recursosWeb = filtrar(JSON.parse(localStorage.getItem("arrayDesarrolloProfesional")), "id_tipo", "2");
@@ -18,9 +18,18 @@ const cargarRecursosWeb=()=>{
     console.log("subcategoriasODP",subcategoriasODP);
 }
 
+
+
 //Componente que renderiza recursos web para docentes
 const CatalogoWeb = (props) => {
-cargarRecursosWeb();
+    const [recursosFiltrados, setRecursosFiltrados]= useState(false);
+
+    cargarRecursosWeb();
+
+    const handleObtenerSubcategoriga=(e)=>{
+        subCategoria = e.target.value;
+        setRecursosFiltrados(  filtrar(recursosWeb, "nombreCategoria", subCategoria )  )
+    }
 
     return ( 
         <React.Fragment>
@@ -31,26 +40,32 @@ cargarRecursosWeb();
              </div>
             </div>
 
-            
-
             <div className="row">
                 <div className="col-sm-3">                
                                 <div className="input-group-prepend">
                                     <label className="input-group-text etiquetas-busquedas" htmlFor="selSubCategoria">Subcategoría</label>
                                 </div>
-                                <select className="custom-select buscadores-materias" id="selSubCategoria"  >
+                                <select 
+                                    className="custom-select buscadores-materias" 
+                                    id="selSubCategoria"
+                                    onClick={handleObtenerSubcategoriga} >
                                     <option defaultValue value="" >Seleccione:</option>
                                             {
                                             subcategoriasODP.map((item, i) => (
-                                                <option key={"niveles" + i} value={item.id}> {item.nombreCategoria } </option>
+                                                <option key={"niveles" + i} value={item.nombreCategoria}> {item.nombreCategoria } </option>
                                             ))}
                                 </select>                
                 </div>
             </div>
             
-            <div id="educativos">  <a href="#inicio">   <h4 className="tit-catalogo" > Sitios Educativos </h4> <br/> </a>    
+            <div id="educativos">  
+                    {subCategoria &&
+                        <h4 className="tit-catalogo" > {subCategoria} </h4> 
+                    }                   
+                    
                         {
-                            recursosWeb.map((item, i) => (
+                            recursosFiltrados && 
+                            recursosFiltrados.map((item, i) => (
                                 <div className="tarjetas-web"  key={i} >
                                 
                                     <div className="row"> 
@@ -64,84 +79,6 @@ cargarRecursosWeb();
                                         <div className="col-11">   
                                         <h6> <b> {item.nombre}</b></h6>
                                             <p className="text-catalogo">  {item.descripcion}  </p>
-                                            <a href={item.url } className="link-card"  target="_blank" rel="noopener noreferrer" ><b>Visitar</b></a>
-                                            <hr/>  <hr/>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            ) )
-                         }
-            </div>
-
-            <div id="web">   <a href="#inicio">    <h4 className="tit-catalogo"> Herramientas Web </h4> <br/> </a> 
-                        {
-                            sitiosWeb[1].map((item, i) => (
-                                <div className="tarjetas-web"  key={i} >
-                                
-                                    <div className="row"> 
-
-                                        <div className="col-1"> 
-                                        <div>
-                                            <img  alt="Desarrollo de otras ofertas" className="img_dec_sitio" src={img.DesarrolloDecSitios}/>
-                                        </div>
-                                        </div>
-                                        
-                                        <div className="col-11">   
-                                        <h6> <b> {item.nombre}</b></h6>
-                                            <p className="text-catalogo">  {item.desc}  </p>
-                                            <a href={item.url } className="link-card"  target="_blank" rel="noopener noreferrer" ><b>Visitar</b></a>
-                                            <hr/>  <hr/>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            ) )
-                         }
-            </div>
-
-            <div id="apps">   <a href="#inicio">   <h4 className="tit-catalogo"> Apps Educativas </h4> <br/> </a>  
-                        {
-                            sitiosWeb[2].map((item, i) => (
-                                <div className="tarjetas-web"  key={i} >
-                                
-                                    <div className="row"> 
-
-                                        <div className="col-1"> 
-                                        <div>
-                                            <img  alt="Desarrollo de otras ofertas" className="img_dec_sitio" src={img.DesarrolloDecSitios}/>
-                                        </div>
-                                        </div>
-                                        
-                                        <div className="col-11">   
-                                        <h6> <b> {item.nombre}</b></h6>
-                                            <p className="text-catalogo">  {item.desc}  </p>
-                                            <a href={item.url } className="link-card"  target="_blank" rel="noopener noreferrer" ><b>Visitar</b></a>
-                                            <hr/>  <hr/>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            ) )
-                         }
-            </div>
-
-            <div id="estrategias">  <a href="#inicio">      <h4 className="tit-catalogo"> Estrategias de aprendizaje</h4> <br/> </a>
-                        {
-                            sitiosWeb[3].map((item, i) => (
-                                <div className="tarjetas-web"  key={i} >
-                                
-                                    <div className="row"> 
-
-                                        <div className="col-1"> 
-                                        <div>
-                                            <img  alt="Desarrollo de otras ofertas" className="img_dec_sitio" src={img.DesarrolloDecSitios}/>
-                                        </div>
-                                        </div>
-                                        
-                                        <div className="col-11">   
-                                        <h6> <b> {item.nombre}</b></h6>
-                                            <p className="text-catalogo">  {item.desc}  </p>
                                             <a href={item.url } className="link-card"  target="_blank" rel="noopener noreferrer" ><b>Visitar</b></a>
                                             <hr/>  <hr/>
                                         </div>
