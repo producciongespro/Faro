096d41280a9ea2aa4a395720de3f1ad3984f88a8
lchaconc <luis.chacon.campos@gmail.com>
2020-08-27 13:53:15 -0600

    REcomodo de secciones

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -16,28 +16,29 @@ var links = dsLinks[0];
 const Home = (props) => {
   return (
     <React.Fragment>
-{/**
-Seincluye una sección con tabindex -1
-       */}
-      <section  tabIndex="-1" title="Área de botones" id="botones">
+      {
+        /** En el area de los botones Se incluye una sección con tabindex -1*/
+      }
+      <section tabIndex="-1" title="Área de botones" id="botones">
         <div role="navigation" className="barra-botones">
-          <img className="btn img-fluid hvr-pop" id="indicaciones" src={img + "caja.png"} onClick={props.showModal} data-typecontent="help" data-content={textos.IndicacionesPortada} alt="Indicaciones" /> <br />
-          <img className="btn img-fluid hvr-pop" id="home" src={img + "volver_portada.png"} data-tar="Portada" onClick={props.changePage} alt="Volver a Portada" /><br />
-          <img className="img-fluid botones-portada hvr-pop" id="uso" src={img + "btn_usocaja.png"} data-typecontent="usoCaja" onClick={props.showModal} data-content={textos.VideoUsoCaja} alt="Uso de la caja de herramientas" /> <br /> <br />
+          <img alt="Indicaciones" className="btn img-fluid hvr-pop" id="indicaciones" src={img + "caja.png"} onClick={props.showModal} data-typecontent="help" data-content={textos.IndicacionesPortada}  /> <br />
+          <img alt="Volver a Portada" className="btn img-fluid hvr-pop" id="home" src={img + "volver_portada.png"} data-tar="Portada" onClick={props.changePage}  /><br />
+          <img alt="Uso de la caja de herramientas" className="img-fluid botones-portada hvr-pop" id="uso" src={img + "btn_usocaja.png"} data-typecontent="usoCaja" onClick={props.showModal} data-content={textos.VideoUsoCaja}  /> <br /> <br />
           <a href={links.Encuesta} target="_blank" rel="noopener noreferrer"  >
-            <img className="img-fluid botones-portada hvr-pop" id="encuesta" src={img + "btn_encuesta.png"} alt="Encuesta" />
+            <img alt="Encuesta" className="img-fluid botones-portada hvr-pop" id="encuesta" src={img + "btn_encuesta.png"}  />
           </a>
           <br />
-          <img className="btn img-fluid hvr-pop incidencias" id="btnIncidencias" data-typecontent="opcIncidencias" onClick={props.showModal} data-infosource={2} onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut} alt="Inicidencias" src={img + "incidencias.png"} /><br />
+          <img alt="Inicidencias o reportes de fallas" className="btn img-fluid hvr-pop incidencias" id="btnIncidencias" data-typecontent="opcIncidencias" onClick={props.showModal} data-infosource={2} onMouseOver={props.onMouseOver} onMouseOut={props.onMouseOut}  src={img + "incidencias.png"} /><br />
           <hr />
-          <img className="btn img-fluid hvr-pop acercade" id="acercade" src={img + "creditos.png"} onClick={props.showModal} data-typecontent="acercaDe" data-content={textos.IndicacionesPortada} alt="Indicaciones" /> <br />
+          <img alt="Información acerca de la caja de herramientas" className="btn img-fluid hvr-pop acercade" id="acercade" src={img + "creditos.png"} onClick={props.showModal} data-typecontent="acercaDe" data-content={textos.IndicacionesPortada}  /> <br />
         </div>
       </section>
-
+      {
+        /** En el area de los vínculos a las diferentes categorías se incluye una section con tabindex -2*/
+      }
+        <section  tabIndex="-2" title="Área de acceso a recurso de la caja"  id="maestra">
       <div className="row">
-        <div className="col-1">
-
-        </div>
+        <div className="col-1"></div>        
         <div className="col-10 ">
           <img alt="maestra" id="principal" src={img + "maestraMenu.png"} />
           <img alt="Apoyos para el clima del aula" data-tar="ApoyoClimaAula" onClick={props.changePage} id="gafas" className="cliqueables" src={img + "gafas.png"} />
@@ -46,12 +47,11 @@ Seincluye una sección con tabindex -1
           <img alt="Recursos didáctico" id="laptop" data-tar="RecursosDidacticos" onClick={props.changePage} className="cliqueables" src={img + "laptop.gif"} />
           <img alt="Desarrollo profesional" id="celular" data-tar="DesarrolloProf" onClick={props.changePage} className="cliqueables2" src={img + "celular.png"} />
           <img alt="Apoyos para el planeamiento" id="bolso" data-tar="ApoyosPlan" onClick={props.changePage} className="cliqueables" src={img + "bolso.png"} />
-        </div>
+        </div>        
         <div className="col-1">
-
         </div>
       </div>
-
+      </section>
     </React.Fragment>
   );
 }
